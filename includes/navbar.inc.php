edf5005a16ea791848c8e54481efadb28a03c202
KhanyisaJG <52782147+KhanyisaJG@users.noreply.github.com>
2020-10-01 17:19:39 +0200

    Showcase the work I have done.
    I created a website.

---
+++ includes/navbar.inc.php
@@ -0,0 +1,28 @@
+<!-- header -->
+	<header id="main_header">
+		<div class="container">
+			<!-- navbar -->
+			<nav>
+				<!-- brand -->
+				<div class="brand">
+					<h1><span>L</span>O<span>N</span>I<span>A</span></h1>
+				</div>
+	            
+	            <!-- nav links -->
+				<ul class="nav_links">
+					<li><a href="index.php" class="pageName current">Work</a></li>
+					<li><a href="about.php" class="pageName">About</a></li>
+					<li><a href="contact.php" class="pageName">Contact</a></li>
+				</ul>
+	            
+	            <!-- burger for side menu -->
+				<div class="burger">
+					<div class="line1"></div>
+					<div class="line2"></div>
+					<div class="line3"></div>
+				</div>
+			</nav>
+			<!-- end navbar -->
+		</div>
+	</header>
+    <!-- end header -->
